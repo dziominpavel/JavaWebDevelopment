879c787464dcfa557cc35ec856527c088d027c2e
DziominPavel <DziominPavel@gmail.com>
2019-08-01 11:31:33 +0300

    validate age

--- Library/js/edit.js
+++ Library/js/edit.js
@@ -3,7 +3,7 @@ function checkAge() {
     var val = document.getElementById('textinputAge').value;
     var len = val.toString().length;
     var valInt = parseInt(val);
-    if (len != 0) {
+    if (len !== 0) {
         if (valInt > 100 || valInt < 1 || isNaN(valInt)) {
             window.alert("Введите корректный возраст");
         }
